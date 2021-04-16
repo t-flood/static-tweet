cb713bc0bd700343fa71f4704a3fa8d9372e06a5
Tim Flood <tflood9@gmail.com>
2021-04-16 11:37:42 -0400

    added buttons to tweet

--- src/index.js
+++ src/index.js
@@ -8,7 +8,14 @@ function Tweet() {
       <Avatar />
       <div className="content">
         <Author />
+        <Time />
         <Message />
+        <div className="buttons">
+          <ReplyButton />
+          <RetweetButton />
+          <LikeButton />
+          <MoreOptionsButton />
+        </div>
       </div>
     </div>
   );
@@ -36,5 +43,13 @@ function Author() {
     </span>
   );
 }
+
+const Time = () => <span className="time">3h ago</span>;
+const ReplyButton = () => <i className="fa fa-reply reply-button" />;
+const RetweetButton = () => <i className="fa fa-retweet retweet-button" />;
+const LikeButton = () => <i className="fa fa-heart like-button" />;
+const MoreOptionsButton = () => (
+  <i className="fa fa-ellipsis-h more-options-button" />
+);
 
 ReactDOM.render(<Tweet />, document.querySelector("#root"));
